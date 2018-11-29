d6a036d5d423065b5ec759d33f5e91e71d16f14e
John Jimenez <mayonesa.cosmica@gmail.com>
2018-11-28 19:52:48 -0500

    added intermediate vals to increase readability

--- src/main/scala/gol/GameOfLife.scala
+++ src/main/scala/gol/GameOfLife.scala
@@ -7,27 +7,29 @@ object GameOfLife {
     val nRows = culture.size
     val nCols = if (nRows > 0) culture(0).size else 0
 
-    def nextGen(i: Int, j: Int) = {
+    def nextLife(i: Int, j: Int) = {
       val alive = culture(i)(j)
       val nAlive = (neigborStart(i) until neighborEnd(i, nRows)).foldLeft(0) { (acc, neighorRowI) =>
-        val nAliveInRow = culture(neighorRowI).slice(neigborStart(j), neighborEnd(j, nCols)).count(identity)
-        acc + nAliveInRow
+        val row = culture(neighorRowI)
+        val neighborRow = row.slice(neigborStart(j), neighborEnd(j, nCols))
+        val nAliveInNeighborRow = neighborRow.count(identity)
+        acc + nAliveInNeighborRow
       }
 
       // don't count yourself
       val nNeighbors = if (alive) nAlive - 1 else nAlive
 
-      nextLife(nNeighbors, alive)
+      nextLifeFrom(nNeighbors, alive)
     }
 
-    Vector.tabulate(nRows, nCols)(nextGen)
+    Vector.tabulate(nRows, nCols)(nextLife)
   }
 
   private def neigborStart(k: Int) = max(0, k - 1)
 
   private def neighborEnd(k: Int, n: Int) = min(n, k + 2)
 
-  private def nextLife(nNeighbors: Int, alive: Boolean) =
+  private def nextLifeFrom(nNeighbors: Int, alive: Boolean) =
     if (alive) nNeighbors == 2 || nNeighbors == 3
     else nNeighbors == 3
 }
